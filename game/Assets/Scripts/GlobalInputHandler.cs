4d742c6fb1224b67771d831abf0a12a048e6a381
Benjamin Lindley <benjameslindley@gmail.com>
2022-12-01 16:28:01 -0600

    fixed camera tracking for all scenes; added scene switching via TAB key

--- game/Assets/Scripts/GlobalInputHandler.cs
+++ game/Assets/Scripts/GlobalInputHandler.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GlobalInputHandler : MonoBehaviour
 {
+    float sceneSwitchCooldownTimer;
+    float sceneSwitchCooldownInterval = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sceneSwitchCooldownTimer = Time.time + sceneSwitchCooldownInterval;
     }
 
     // Update is called once per frame
@@ -15,6 +19,30 @@ public class GlobalInputHandler : MonoBehaviour
     {
         if (Input.GetKeyDown("escape")) {
             Application.Quit();
+        }
+
+        if (Input.GetKeyDown("tab")) {
+            if (Time.time > sceneSwitchCooldownTimer) {
+                string[] sceneList = new string []
+                {
+                    "Grasslands",
+                    "Bayou Forest",
+                    "Desert",
+                    "Deciduous Forest",
+                    "Tundra",
+                    "Vampire Area",
+                    "Werewolves Den",
+                };
+
+                string currentScene = SceneManager.GetActiveScene().name;
+                for (int i = 0; i < sceneList.Length; i++) {
+                    if (currentScene == sceneList[i]) {
+                        int nextSceneIndex = (i + 1) % sceneList.Length;
+                        string nextScene = sceneList[nextSceneIndex];
+                        SceneManager.LoadScene(nextScene);
+                    }
+                }
+            }
         }
     }
 }
